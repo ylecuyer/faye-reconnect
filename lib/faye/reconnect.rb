e47b8b6b45cdcdfb8a3e8790384b842939d54285
Adrien Siami <adrien.siami@dimelo.com>
2014-11-14 14:54:41 +0100

    Initial commit with bundle gem

---
+++ lib/faye/reconnect.rb
@@ -0,0 +1,7 @@
+require "faye/reconnect/version"
+
+module Faye
+  module Reconnect
+    # Your code goes here...
+  end
+end
---
+++ lib/faye/reconnect/version.rb
@@ -0,0 +1,5 @@
+module Faye
+  module Reconnect
+    VERSION = "0.0.1"
+  end
+end
