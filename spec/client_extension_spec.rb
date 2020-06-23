614fdb5ec30f71f54682c0ec4a084148ba0375dd
DrissTM <driss.tahraouimaldague@gmail.com>
2020-06-23 12:48:31 +0200

    Expose method to close the connection to redis

--- spec/client_extension_spec.rb
+++ spec/client_extension_spec.rb
@@ -3,7 +3,6 @@ require 'faye/reconnect'
 require 'spec_helper'
 
 describe Faye::Reconnect::ClientExtension do
-
   it 'requires a name option' do
     expect {
       Faye::Reconnect::ClientExtension.new
@@ -13,4 +12,11 @@ describe Faye::Reconnect::ClientExtension do
     }.to_not raise_error
   end
 
+  describe '#close_redis_connection' do
+    it 'calls #close_connection on the redis client' do
+      faye_reconnect = Faye::Reconnect::ClientExtension.new(name: 'foobar')
+      expect_any_instance_of(EventMachine::Hiredis::Client).to receive(:close_connection).and_return(true)
+      faye_reconnect.close_redis_connection
+    end
+  end
 end
